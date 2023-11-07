5dc1329009dc3af4ef60cd2a0e0a569d1be5b61d
rujool11 <132924872+rujool11@users.noreply.github.com>
2023-11-07 20:47:35 +0530

    working prototype

--- index.js
+++ index.js
@@ -1,29 +1,96 @@
-let operator;
-let previousNum;
-let currentNum;
+let operator="";
+let previousValue="";
+let currentValue="";
 
 document.addEventListener("DOMContentLoaded", () => {
     //storing all components when document is loaded
-    let display = document.querySelector(".display");
     let clear = document.querySelector(".clear");
     let decimal = document.querySelector(".decimal");
     let equal = document.querySelector(".equals");
 
-    let numbers = document.querySelectorAll("number");
-    let operators = document.querySelectorAll("operator");
+    let numbers = document.querySelectorAll(".number");
+    let operators = document.querySelectorAll(".operator");
 
-    let previousDisplay = document.querySelector("previous");
-    let currentDisplay = document.querySelector("current");
+    let previousDisplay = document.querySelector(".previous");
+    let currentDisplay = document.querySelector(".current");
 
     numbers.forEach((number) => {
         number.addEventListener("click", (event) => {
-            handleClick(event.target.textContent);
-            //handleClick(e.target.textContent);
+            handleNumber(event.target.textContent);
+            currentDisplay.textContent = currentValue;
         });
+    });
+
+    operators.forEach((op) => {
+        op.addEventListener("click", (event) => {
+            handleOperator(event.target.textContent);
+            previousDisplay.textContent = previousValue + " " + operator;
+            currentDisplay.textContent = currentValue;
+        });
+    });
+
+    clear.addEventListener("click", () => {
+        operator = "";
+        previousValue = "";
+        currentValue = "";
+        previousDisplay.textContent = previousValue;
+        currentDisplay.textContent = currentValue;
+    });
+
+    decimal.addEventListener("click", () => {
+        currentValue += "."
+        currentDisplay.textContent = currentValue;
+    });
+
+    equal.addEventListener("click", () => {
+        let ans = calculate();
+
+        if (ans.toString().length>=10){
+            ans = ans.toFixed(3);
+        }
+
+        previousDisplay.textContent = previousValue + " " + operator + " " + currentValue;
+        currentDisplay.textContent = ans.toString();
+        currentValue = currentDisplay.textContent;
+        operator = "";
+        previousValue = "";
+
     });
 
 });
 
-function handleClick(button) {
-    alert(button);
+function handleNumber(button) {
+    if (currentValue.length>=5){return;}
+    currentValue += button; 
+}
+
+function handleOperator(button) {
+    operator = button;
+    previousValue = currentValue;
+    currentValue = ""
+}
+
+function calculate() {
+    let num1 = Number(previousValue);
+    let num2 = Number(currentValue);
+    let ans;
+    
+    switch (operator) {
+        case "/":
+            ans = num1/num2; 
+            break;
+        case "x":
+            ans = num1*num2;
+            break;
+        case "+":
+            ans = num1+num2;
+            break;
+        case "-":
+            ans = num1-num2;
+            break;
+        default:
+            alert("please enter valid value and operator"); 
+            break;
+    }
+    return ans;
 }
